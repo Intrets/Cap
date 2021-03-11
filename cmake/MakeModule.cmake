11cce9befd7d1c21dc670a34e1a7bc25b02cdabb
Intrets <geen.inspiratie824@gmail.com>
2021-03-11 21:36:50 +0100

    moved source file locations to make intellisense happy

--- cmake/MakeModule.cmake
+++ cmake/MakeModule.cmake
@@ -1,14 +1,14 @@
 function(make_module MODULE_NAME MODULE_FILES)
 	foreach(FILE ${MODULE_FILES})
-		list(APPEND SOURCES_LIST "src/${FILE}.cpp")
-		list(APPEND HEADERS_LIST "${CMAKE_CURRENT_SOURCE_DIR}/include/${MODULE_NAME}/${FILE}.h")
+		list(APPEND SOURCES_LIST "${FILE}.cpp")
+		list(APPEND HEADERS_LIST "include/${MODULE_NAME}/${FILE}.h")
 	endforeach()
 
 	add_library(${MODULE_NAME} ${SOURCES_LIST} ${HEADERS_LIST})
 	add_library("${MODULE_NAME}::${MODULE_NAME}" ALIAS ${MODULE_NAME})
 
-	target_include_directories(${MODULE_NAME} PUBLIC ./include)
-	target_include_directories(${MODULE_NAME} PUBLIC "./include/${MODULE_NAME}")
+	target_include_directories(${MODULE_NAME} PUBLIC include)
+	target_include_directories(${MODULE_NAME} PRIVATE include/${MODULE_NAME})
 
 	target_compile_features(${MODULE_NAME} PUBLIC cxx_std_17)
 
@@ -18,3 +18,4 @@ function(make_module MODULE_NAME MODULE_FILES)
 		FILES ${HEADERS_LIST}
 	)
 endfunction()
+
