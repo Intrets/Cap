fff40951f6950d07be48a0f8591ea46cb0c22815
Intrets <geen.inspiratie824@gmail.com>
2021-03-12 18:28:05 +0100

    can pass the resources folder to the executable by command line argument

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+#include <filesystem>
+
 #include "MainLoop.h"
 #include "Setup.h"
 
@@ -9,7 +12,24 @@ bool OPENGL_DEBUG = true;
 
 GLFWwindow* window;
 
-int main() {
+int main(int argc, char* argv[]) {
+	std::filesystem::path resourcesPath;
+	if (argc > 1) {
+		resourcesPath = std::filesystem::path(argv[1]);
+	}
+	else {
+		std::cout << "No custom path specified, assuming current dir has the resources folder.\n";
+		std::cout << "Can take a single nameless argument to set resources folder.";
+		resourcesPath = std::filesystem::current_path().append("resources");
+	}
+
+	std::cout << "Resources path: " << std::filesystem::absolute(resourcesPath) << "\n";
+
+	if (!std::filesystem::exists(resourcesPath)) {
+		std::cerr << "Fatal error: resources path does not specify an existing folder.\n";
+		return 0;
+	}
+
 	initGLFW(window, OPENGL_DEBUG);
 	initManagers();
 	mainLoop(window);
