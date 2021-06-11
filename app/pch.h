5ae47ecb67b97126fe379c6ef2abea64882835b9
Intrets <geen.inspiratie824@gmail.com>
2021-06-11 17:15:26 +0200

    actually included the precompiled header and fixed typo

---
+++ app/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <wglm/glm.hpp>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <fstream>
+#include <filesystem>
+#include <mem/Locator.h>
+#include <mem/ReferenceManager.h>
