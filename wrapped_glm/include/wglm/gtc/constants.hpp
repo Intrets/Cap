e5e03faf44af9f2336ba201ea76da981583818e9
Intrets <geen.inspiratie824@gmail.com>
2021-07-06 02:52:21 +0200

    removed in-project wrapped_glm

--- wrapped_glm/include/wglm/gtc/constants.hpp
+++
@@ -1,8 +0,0 @@
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable: 4201; disable: 4324)
-#endif
-#include <glm/gtc/constants.hpp>
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
--- wrapped_glm/include/wglm/gtx/transform.hpp
+++
@@ -1,8 +0,0 @@
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable: 4201)
-#endif
-#include <glm/gtx/transform.hpp>
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
